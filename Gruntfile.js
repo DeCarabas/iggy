d2d6d63a5febcfd85220cd353fc8f50c198b62b7
John Doty <jddoty@gmail.com>
2014-08-24 11:39:31 -0700

    Cleaning

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,22 @@
 module.exports = function(grunt) {
+
+  var bindir = 'bin/igweb';
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    bindir: 'bin/igweb',
+
+    clean: {
+      build: {
+        src: ['<%= bindir %>']
+      }
+    },
 
     concat: {
       dnd4: {
         src: ['src/igweb/js/dnd4/**/*.js'],
-        dest: 'bin/igweb/js/dnd4data.js'
+        dest: '<%= bindir %>/js/dnd4data.js'
       },
     },
 
@@ -14,13 +25,13 @@ module.exports = function(grunt) {
     },
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('build', ['concat:dnd4']);
-
-  grunt.registerTask('test', ['qunit']);  
+  grunt.registerTask('test',  ['qunit']);  
 
   grunt.registerTask('default', ['build', 'test']);
 };
